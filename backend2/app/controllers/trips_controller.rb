e6284a0d8cccee0d301d193346ef326079fb6bb5
github username <github email address>
2020-07-21 16:20:49 -0400

    formated json data in trips controller

--- backend2/app/controllers/trips_controller.rb
+++ backend2/app/controllers/trips_controller.rb
@@ -1,6 +1,6 @@
 class TripsController < ApplicationController
     def index
         trips = Trip.all 
-        render json: trips.to_json(include: [:attractions, :hotels])
+        render json: trips.to_json(include: [:attractions, :hotels, :type, :area])
     end 
 end
