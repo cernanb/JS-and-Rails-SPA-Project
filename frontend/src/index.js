548ef9b983aec9aab8fd60eb2f2000615e2d3591
github username <github email address>
2020-07-22 14:08:11 -0400

    rendered data from the rails api in html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,37 @@
 const BASE_URL = "http://localhost:3000"
 const TRIPS_URL = `${BASE_URL}/trips`
 
-// fetch(TRIPS_URL)
-//   .then((res) => res.json())
-//   .then((res) => displayTrips(res))
+fetch(TRIPS_URL)
+  .then((res) => res.json())
+  .then((trips) => displayTrips(trips))
 
-const displayTrips = (e) => {
-    e.preventDefault()
-    const configObject = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify({ type_id: e.target.types.value,
-                               area_id: e.target.areas.value                                       
-                            })
-    }
-    fetch(TRIPS_URL, configObject)
-        .then((res) => res.json())
-        .then((trips) => 
-            trips.forEach(trip => {
-                const main = document.querySelector("main")
-                const tripCity = document.createElement("p")
-                tripCity.innerHTML = trip.city 
-                const tripCounrty = document.createElement("p")
-                tripCounrty.innerHTML = trip.country
+const displayTrips = (trips) => {
+    
+    trips.forEach(trip => {
+        const main = document.querySelector("main")
+        const tripContainer = document.createElement("div")
+        tripContainer.className = "trip-container"
+        const tripCity = document.createElement("p")
+        tripCity.innerHTML = trip.city 
+        const tripCounrty = document.createElement("p")
+        tripCounrty.innerHTML = trip.country
+        const tripType = document.createElement("p")
+        tripType.innerHTML = `Trip type: ${trip.type}`
+        const tripArea = document.createElement("p")
+        tripArea.innerHTML = `Trip area: ${trip.area}`
+        const ul = document.createElement("ul")
 
-                main.appendChild(tripCity)
-                main.appendChild(tripCounrty)
-            })
-    )
+        trip.attractions.forEach(attraction => {
+            const li = document.createElement("li")
+            li.innerHTML = `${attraction.name}`
+            console.log(li) 
+            ul.appendChild(li)
+        })
+        tripContainer.appendChild(tripCity)
+        tripContainer.appendChild(tripCounrty)
+        tripContainer.appendChild(ul)
+        main.appendChild(tripContainer)
+    })
 }  
 
 const form = document.getElementById("main-form")
